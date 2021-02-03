db7bfec39758478299deea2965572cc90298668c
Shubham Chandgude <35603706+spchandgude@users.noreply.github.com>
2021-02-03 21:32:45 +0530

    Update sha.h
    Commented previous function and declared operator overloading.
    Declared hash160 class.

--- src/sha.h
+++ src/sha.h
@@ -2,6 +2,20 @@
 #define bhaldar_H
 #pragma once
 
-bool isHashSmallerThanOrEqualTo(char h1[40], char h2[40]);
+
+
+class hash160{
+        
+    public:
+         char h[40];
+         void setter();     // method to set hashvalue
+         char* getter();     // method to get hashvalue
+
+};
+
+//bool isHashSmallerThanOrEqualTo(char h1[40], char h2[40]);
+
+bool operator <= (hash160 const &obj1, hash160 const &obj2);   // Operator overloading
+
 
 #endif 
